03fd910cd34f08a2e49d47ffabaf5c8e7a903f99
natebecker11 <natebecker11@gmail.com>
2018-11-10 12:56:53 -0500

    retooled ORM as revealing module

---
+++ portfoliomaker/orm.js
@@ -0,0 +1,67 @@
+const connection = require('../config/connection')
+
+const orm = (() => {
+
+  const questions = num => Array(num).fill('?').toString();
+
+  const sqlVals = object => {
+      let arrPairs = Object.entries(object)
+      return arrPairs.map(x => `${x[0]} = '${x[1]}'`).join(', ');
+  }
+
+  const selectAll = table => {
+      return new Promise((resolve, reject) => {
+          let queryString = `SELECT * FROM ${table};`;
+          connection.query(queryString, (err, res) => {
+              if (err) throw err;
+              resolve(res)
+          })
+      })
+  }
+
+  const selectSome = (table, ...cols) => {
+    return new Promise((resolve, reject) => {
+        let colList = cols.join(', ')
+        let queryString = `SELECT ${colList} FROM ${table}`
+        connection.query(queryString, (err, res) => {
+          if (err) throw err
+          resolve(res)
+        })
+      }
+    )
+  }
+
+  const insertOne = (table, cols, vals) => {
+      return new Promise((resolve, reject) => {
+          let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${questions(vals.length)});`;
+          console.log(queryString)
+          connection.query(queryString, vals, (err, res) => {
+              if (err) throw err;
+              resolve(res);
+          })
+      })
+  },
+
+  const updateOne = (table, vals, condition) => {
+      return new Promise((resolve, reject) => {
+          let queryString = `UPDATE ${table} SET ${sqlVals(vals)} WHERE ${condition};`;
+          connection.query(queryString, (err, res) => {
+              if (err) throw err;
+              resolve(res);
+          })
+      })
+  }
+
+  return {
+    selectAll: selectAll,
+    selectSome: selectSome,
+    selectOne: selectOne,
+    insertOne: insertOne,
+    updateOne, updateOne
+  }
+
+})
+
+
+module.exports = orm;
+
