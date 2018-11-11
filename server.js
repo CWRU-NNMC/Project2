052f4d00945a30b89948c62a8032261c2e29d2c2
Nathan Golba <ngolba@gmail.com>
2018-11-11 01:11:34 -0500

    File Restructure

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('./dist'))
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  })
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT);
