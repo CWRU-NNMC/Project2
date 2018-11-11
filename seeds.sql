ea84cc830b14943320c68a409d09716d359f84cc
natebecker11 <natebecker11@gmail.com>
2018-11-11 15:09:08 -0500

    database seeds for testing

---
+++ seeds.sql
@@ -0,0 +1,37 @@
+USE usysutc9ey5bm5dx;
+
+-- INSERT INTO users (username, email, pw, preferences)
+-- VALUES ('colin', 'colin@nate.com', 'aaccaa', '{"default" : "true"}'),
+-- ('mike', 'mike@nate.com', 'qwerty', '{"default" : "true"}'),
+-- ('nathan', 'nathan@nate.com', 'golba', '{"default" : "true"}'),
+-- ('dusty', 'dustin@nate.com', 'doggos', '{"default" : "true"}');
+
+
+-- INSERT INTO portfolios (usersid, name, config, description, technologies) 
+-- VALUES ('1', 'designedbynate', '{"config": "default"}', 'My design portfolio', 1011000001),
+-- ('2', 'colincodes', '{"config": "default"}', 'Sweet lil coding portfolio', 1011000001),
+-- ('3', 'madebymike', '{"config": "default"}', 'Portfolio of projects', 1101000001),
+-- ('4', 'ngolba', '{"config": "default"}', 'A bunch of projects', 1011110111);
+
+
+-- INSERT INTO projects (usersid, portfolioid, imageurl, githuburl, description) 
+-- VALUES ('1', '2', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My other thing'),
+-- ('1', '3', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My silly thing'),
+-- ('2', '4', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My awesome thing'),
+-- ('2', '4', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My dumb thing'),
+-- ('3', '5', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My impressive thing'),
+-- ('3', '5', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My super thing'),
+-- ('3', '5', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My dingy thing'),
+-- ('4', '6', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My little thing'),
+-- ('4', '6', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My giant thing'),
+-- ('4', '6', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My happy thing'),
+-- ('1', '3', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My huge thing'),
+-- ('1', '2', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'http://www.github.com', 'My nice thing');
+
+
+-- INSERT INTO templates (description, defaultconfig) 
+-- VALUES ('A very modern design', '{"json": "stuff"}'),
+-- ('A minimalist look', '{"json": "stuff"}'),
+-- ('A grayscale nightmare', '{"json": "stuff"}');
+
+
