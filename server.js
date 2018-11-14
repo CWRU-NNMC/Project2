f8ef6d433ca49274a1378f613bf5709da6846ca8
natebecker11 <natebecker11@gmail.com>
2018-11-14 15:32:38 -0500

    more routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const  { userPageFunction, portfolioPageFunction, checkUserName, checkPortfolioName, addNewUser, addNewPortfolio } = require('./db/dbLib')
+const  { userPageFunction, portfolioPageFunction, checkUserName, checkPortfolioName, addNewUser, addNewPortfolio, addNewProject } = require('./db/dbLib')
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
@@ -12,28 +12,28 @@ app.use(express.static('./dist'))
 // ** "GET" routes that are actually written as POST routes because of Vue-reasons
 
 
-// This route checks whether a user name is available for use. Returns true if available.
+// checks whether a user name is available for use. Returns true if available.
 app.post('/api/user/query/:name', (req, res) => {
     checkUserName(req.params.name)
         .then(reply => res.send(reply))
         .catch(err => res.status(500).send(err))
 })
 
-// This route checks whether a portfolio name is available for use. Returns true if available.
+// checks whether a portfolio name is available for use. Returns true if available.
 app.post('/api/portfolio/query/:name', (req, res) => {
     checkPortfolioName(req.params.name)
         .then(reply => res.send(reply))
         .catch(err => res.status(500).send(err))
 })
 
-// This route returns an object with all relevant information on a user
+// returns an object with all relevant information on a user
 app.post('/api/user/:name', (req, res) => {
     userPageFunction(req.params.name)
         .then(json => res.status(200).send(json))
         .catch(err => res.status(err.code).send(err.message))
 })
 
-// This route returns an object with all the relevant information on a portfolio
+// returns an object with all the relevant information on a portfolio
 app.post('/api/portfolio/:name', (req, res) => {
     portfolioPageFunction(req.params.name)
         .then(json => res.status(200).send(json))
@@ -42,8 +42,8 @@ app.post('/api/portfolio/:name', (req, res) => {
 
 // ** "POST" routes for adding to the database
 
-// This adds a new user to the DB
-app.post('/api/add/user/:name', (req, res) => {
+// adds a new user to the DB
+app.post('/api/manage/user/:name', (req, res) => {
     checkUserName(req.params.name)
     .then(reply => {
         if (!reply) throw new Error('This username is unavailable.')
@@ -53,8 +53,8 @@ app.post('/api/add/user/:name', (req, res) => {
     .catch(err => res.status(500).send(`${err}`))
 })
 
-// This adds a new portfolio to the DB
-app.post('/api/add/portfolio/:name', (req, res) => {
+// adds a new portfolio to the DB
+app.post('/api/manage/portfolio/:name', (req, res) => {
     checkPortfolioName(req.params.name)
     .then(reply => {
         if (!reply) throw new Error('This Portfolio Name is unavailable.')
@@ -64,10 +64,24 @@ app.post('/api/add/portfolio/:name', (req, res) => {
     .catch(err => res.status(500).send(`${err}`))
 })
 
-
+// adds a new project to the DB
+app.post('/api/manage/project/:portfolioname', (req, res) => {
+    addNewProject(req.body)
+    .then(results => res.send(results))
+    .catch(err => res.status(500).send(`${err}`))
+})
 
 
 // ** "PUT" routes for updating the database
+
+// updates existing user info
+app.put('/api/manage/user/:name')
+
+// updates existing portfolio info
+app.put('/api/manage/portfolio/:name')
+
+// updates existing project info
+app.put('/api/manage/project/:id')
 
 // ** "DELETE" routes for deleting from the database
 
