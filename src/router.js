fd7e7ea3d43d2fc5f2e77109d2e841f6883af90b
Nathan Golba <ngolba@gmail.com>
2018-11-14 16:34:15 -0500

    Route/Store updating WIP

--- src/router.js
+++ src/router.js
@@ -3,8 +3,17 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import User from './views/User.vue'
 import Portfolio from './views/Portfolio.vue'
-// import Login from './views/Login.vue' 
+import Login from './views/Login.vue' 
+import Signup from './views/Signup.vue'
+import Creator from './views/Creator.vue'
+import InvalidPortfolio from './views/InvalidPortfolio.vue'
+import PortConstruction from './views/PortConstruction.vue'
+import CreatorBio from './components/CreatorBio.vue' 
+import CreatorChooseTemplate from './components/CreatorChooseTemplate.vue' 
+import CreatorProject from './components/CreatorProject.vue' 
+import CreatorSkillsEdu from './components/CreatorSkillsEdu.vue' 
 import store from './store'
+
 
 Vue.use(Router)
 
@@ -18,23 +27,64 @@ export default new Router({
     },
     {
       path: '/user/:id',
+      name: 'user',
       component: User,
       beforeEnter: (to, from, next) => {
         // if (user authorized to see page) {
-        store.dispatch('getPageJson', {to}).then(() => next())
+        store.dispatch('getPageJson', {to}).then(res => res ? next() : next('/invalid-user'))
         // }
         // else next('/login')
       }
     },
     {
+      path: '/invalid-portfolio',
+      name: 'invalidportfolio',
+      component: InvalidPortfolio
+    },
+    {
+      path: '/construction',
+      name: 'underconstruction', 
+      component: PortConstruction
+    },
+    {
       path: '/portfolio/:name',
+      name: 'portfolio',
       component: Portfolio,
       beforeEnter: (to, from, next) => {
         // if (user authorized to see page) {
-        store.dispatch('getPageJson', {to}).then(() => next())
+        store.dispatch('getPageJson', {to}).then(res => {
+          if(!res) next('/invalid-portfolio')
+          else if (store.getters.getPageHidden) next('/construction')
+          else next()
+        })
         // }
         // else next('/')
       }
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    },
+    {
+      path: '/portfolio-creator',
+      name: 'creator',
+      component: Creator,
+      children: [
+        { path: 'bio', component: CreatorBio },
+        { path: 'skills', component: CreatorSkillsEdu },
+        { path: 'projects', component: CreatorProject },
+        { path: 'templates', component: CreatorChooseTemplate}
+      ]
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
   ]
 })
--- src/store.js
+++ src/store.js
@@ -12,14 +12,20 @@ export default new Vuex.Store({
         userName: false,
         userId: 0,
         currentPageJson: {},
+        nameAvailable: false
     },
     mutations: {
         setPage(state, data) {
             state.currentPageJson = data
+        },
+        setNameAvailable(state, data) {
+            state.nameAvailable = data
         }
     },
     getters: {
-        getPageInfo: state => state.currentPageJson
+        getPageInfo: state => state.currentPageJson,
+        getPageHidden: state => state.currentPageJson.public,
+        getNameAvailable: state => state.nameAvailable
     },
     actions: {
         getPageJson(context, {to}) {
@@ -27,9 +33,17 @@ export default new Vuex.Store({
                 let queryString = `/api${to.fullPath}`
                 axios.post(queryString, to.id).then(({data}) => {
                     context.commit('setPage', {data})
-                }).then(() => resolve())
-                .catch(() => reject())
+                }).then(() => resolve(true))
+                .catch((error) => {
+                    console.log(error); 
+                    resolve(false)})   /// this resolves if user/portfolio doesn't exist. router calling it checks for boolean
             })
-        }
+        },
+        /////////// this will require using the getter function as store.dispatch('checkNameAvailable', name).then(() => store.getters.getNameAvailable)
+        checkNameAvailable (context, {name, pageType}) {
+            let queryString = `/api/${pageType}/query/${name}`
+            axios.post(queryString, name).then(res => context.commit('setNameAvalable', {res}))
+        },
+        //////// add user/portfolio action happens here
     }
 })
