76f3c5b3dec56b9f687fe11f0d86f43b437f3418
BikeMixel <bixelmike@gmail.com>
2018-11-15 18:53:23 -0500

    blah blah sign up form

--- src/views/Sign-Up.vue
+++ src/views/Sign-Up.vue
@@ -15,39 +15,41 @@
 							</div>
 						</v-card-title>
                         <v-container>
-								<v-form class = 'form'>
-									<v-text-field
-                                        name= "username"
-                                        label= "Choose a username"
-                                    > 
-									</v-text-field>
-									<v-text-field
-                                        name= "password"
-                                        label= "Choose a password"
+								<div>
+                                    <v-tabs
+                                        centered
+                                        color="blue lighten-2"
+                                        dark
+                                        icons-and-text
                                     >
-									</v-text-field>
-                                    <v-text-field
-                                        name="email"
-                                        label= "Enter your email address"
-                                    > 
-									</v-text-field>
-									<v-text-field
-                                        name= "image"
-                                        label= "We need a way to have user upload their image"
-                                    >
-									</v-text-field>
-                                    <v-text-field
-                                        name= "dummy1"
-                                        label= "Some other shit we need"
-                                    > 
-									</v-text-field>
-									<v-text-field
-                                        name= "dummy2"
-                                        label= "More shit"
-                                    >
-									</v-text-field>
-                                    <v-btn @click="">Sign Up </v-btn> 
-								</v-form>
+                                        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+                                        <v-tab href="#tab-1">
+                                        Basic Information
+                                        <v-icon>accessibility_new</v-icon>
+                                        </v-tab>
+
+                                        <v-tab href="#tab-2">
+                                        GitHub
+                                        <v-icon>swap_horiz</v-icon>
+                                        </v-tab>
+
+                                        <v-tab href="#tab-3">
+                                        Pick a Style
+                                        <v-icon>opacity</v-icon>
+                                        </v-tab>
+
+                                        <v-tab-item
+                                        v-for="i in 3"
+                                        :id="'tab-' + i"
+                                        :key="i"
+                                        >
+                                        <v-card flat>
+                                            <v-card-text></v-card-text>
+                                        </v-card>
+                                        </v-tab-item>
+                                    </v-tabs>
+                                </div>
 							</v-container>
                         </v-card>
                     </v-flex>
@@ -56,6 +58,33 @@
         </v-content>
 	</v-app>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        active: null,
+        forms: [
+            {
+                text: "General Info"
+            },
+            {
+                text: "GitHub"
+            },
+            {
+                text: "Choose a Style"
+            }
+        ] 
+      }
+    },
+    methods: {
+      next () {
+        const active = parseInt(this.active)
+        this.active = (active < 2 ? active + 1 : 0)
+      }
+    }
+  }
+</script>
 
 <style scoped>
     .form {
